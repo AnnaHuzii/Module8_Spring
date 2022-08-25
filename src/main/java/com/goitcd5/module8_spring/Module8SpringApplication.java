5ec87773c25ee47798d907e31df1b8135b2cd400
AnnaHuzii <anna_guzii@ukr.net>
2022-08-26 01:17:33 +0300

    version1

---
+++ src/main/java/com/goitcd5/module8_spring/Module8SpringApplication.java
@@ -0,0 +1,13 @@
+package com.goitcd5.module8_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module8SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module8SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goitcd5/module8_spring/Module8SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goitcd5.module8_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module8SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
