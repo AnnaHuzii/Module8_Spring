a09220941bdd8b694f4648fb6a083d544f0e79bc
AnnaHuzii <anna_guzii@ukr.net>
2022-09-13 02:46:29 +0300

    prav2

--- src/main/java/com/goitcd5/module8_spring/Module8SpringApplication.java
+++ src/main/java/com/goitcd5/module8_spring/Module8SpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Module8SpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Module8SpringApplication.class, args);
     }
